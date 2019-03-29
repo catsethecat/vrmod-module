57c5a0d06f743365bfa8e63e5948fea34f5abc83
Catse <45233053+catsethecat@users.noreply.github.com>
2019-03-29 14:47:42 +0200

    replaced hardcoded actions with dynamic handling

--- src/source.cpp
+++ src/source.cpp
@@ -15,41 +15,34 @@
 //*************************************************************************
 
 //openvr related
-vr::IVRSystem * pSystem = NULL;
-vr::IVRInput * pInput = NULL;
-vr::TrackedDevicePose_t poses[vr::k_unMaxTrackedDeviceCount];
-//todo: handle actions dynamically
-vr::VRActionSetHandle_t actionSet			= vr::k_ulInvalidActionSetHandle;
-vr::VRActiveActionSet_t activeActionSet		= { 0 };
-vr::VRActionHandle_t boolean_primaryfire	= vr::k_ulInvalidActionHandle;
-vr::VRActionHandle_t boolean_secondaryfire	= vr::k_ulInvalidActionHandle;
-vr::VRActionHandle_t boolean_changeweapon	= vr::k_ulInvalidActionHandle;
-vr::VRActionHandle_t boolean_use			= vr::k_ulInvalidActionHandle;
-vr::VRActionHandle_t boolean_spawnmenu		= vr::k_ulInvalidActionHandle;
-vr::VRActionHandle_t pose_lefthand			= vr::k_ulInvalidActionHandle;
-vr::VRActionHandle_t pose_righthand			= vr::k_ulInvalidActionHandle;
-vr::VRActionHandle_t vector2_walkdirection	= vr::k_ulInvalidActionHandle;
-vr::VRActionHandle_t boolean_walk			= vr::k_ulInvalidActionHandle;
-vr::VRActionHandle_t boolean_flashlight		= vr::k_ulInvalidActionHandle;
-vr::VRActionHandle_t boolean_turnleft		= vr::k_ulInvalidActionHandle;
-vr::VRActionHandle_t boolean_turnright		= vr::k_ulInvalidActionHandle;
-vr::VRActionHandle_t boolean_mic			= vr::k_ulInvalidActionHandle;
-vr::VRActionHandle_t boolean_reload			= vr::k_ulInvalidActionHandle;
-vr::VRActionHandle_t boolean_undo			= vr::k_ulInvalidActionHandle;
+typedef struct {
+	vr::VRActionHandle_t handle = vr::k_ulInvalidActionHandle;
+	char fullname[256];
+	char type[256];
+	char* name;
+}action;
+vr::IVRSystem*			g_pSystem = NULL;
+vr::IVRInput*			g_pInput = NULL;
+vr::TrackedDevicePose_t g_poses[vr::k_unMaxTrackedDeviceCount];
+vr::VRActionSetHandle_t g_actionSet = vr::k_ulInvalidActionSetHandle;
+vr::VRActiveActionSet_t g_activeActionSet = { 0 };
+action					g_actions[64];
+int						g_actionCount = 0;
+
 
 //directx
-ID3D11DeviceContext* d3d11Context;
-ID3D11Device* d3d11Device;
-ID3D11Texture2D* d3d11Texture = NULL;
-IDirect3DDevice9* d3d9Device;
-HANDLE sharedTexture = NULL;
+ID3D11DeviceContext*	g_d3d11Context;
+ID3D11Device*			g_d3d11Device;
+ID3D11Texture2D*		g_d3d11Texture = NULL;
+IDirect3DDevice9*		g_d3d9Device;
+HANDLE					g_sharedTexture = NULL;
 
 //other
-float horizontalFOV = 0;
-float horizontalOffset = 0;
-float calculatedHorizontalOffset = 0;
-uint32_t recommendedWidth = 0;
-uint32_t recommendedHeight = 0;
+float		g_horizontalFOV = 0;
+float		g_horizontalOffset = 0;
+float		g_calculatedHorizontalOffset = 0;
+uint32_t	g_recommendedWidth = 0;
+uint32_t	g_recommendedHeight = 0;
 
 //*************************************************************************
 //                         CreateTexture Hook
@@ -59,8 +52,8 @@ typedef HRESULT(APIENTRY* CreateTexture) (IDirect3DDevice9*, UINT, UINT, UINT, D
 CreateTexture CreateTexture_orig = 0;
 
 HRESULT APIENTRY CreateTexture_hook(IDirect3DDevice9* pDevice, UINT w, UINT h, UINT levels, DWORD usage, D3DFORMAT format, D3DPOOL pool, IDirect3DTexture9** tex, HANDLE* shared_handle) {
-	if (sharedTexture == NULL) {
-		shared_handle = &sharedTexture;
+	if (g_sharedTexture == NULL) {
+		shared_handle = &g_sharedTexture;
 		pool = D3DPOOL_DEFAULT;
 	}
 	return CreateTexture_orig(pDevice, w, h, levels, usage, format, pool, tex, shared_handle);
@@ -94,13 +87,13 @@ void HookDirectX() {
 	params.hDeviceWindow = window;
 	params.BackBufferFormat = D3DFMT_UNKNOWN;
 
-	if (dx->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, window, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &params, &d3d9Device) != D3D_OK) {
+	if (dx->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, window, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &params, &g_d3d9Device) != D3D_OK) {
 		MessageBoxA(NULL, "CreateDevice", NULL, MB_OK);
 		return;
 	}
 
 	//add the hook
-	DWORD* dVtable = (DWORD*)d3d9Device;
+	DWORD* dVtable = (DWORD*)g_d3d9Device;
 	dVtable = (DWORD*)dVtable[0];
 
 	CreateTexture_orig = (CreateTexture)dVtable[23];
@@ -131,28 +124,28 @@ void HookDirectX() {
 //                           LUA VRMOD_MirrorFrame
 //*************************************************************************
 LUA_FUNCTION(VRMOD_MirrorFrame) {
-	if (sharedTexture == NULL)
+	if (g_sharedTexture == NULL)
 		return 0;
 
-	if (d3d11Texture == NULL) {
+	if (g_d3d11Texture == NULL) {
 		//create dx11 device
-		if (D3D11CreateDevice(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, 0, NULL, NULL, D3D11_SDK_VERSION, &d3d11Device, NULL, &d3d11Context) != S_OK) {
+		if (D3D11CreateDevice(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, 0, NULL, NULL, D3D11_SDK_VERSION, &g_d3d11Device, NULL, &g_d3d11Context) != S_OK) {
 			MessageBoxA(NULL, "D3D11CreateDevice", NULL, MB_OK);
 			return 0;
 		}
 		//get dx11 texture from shared texture handle
 		ID3D11Resource* res;
-		if (FAILED(d3d11Device->OpenSharedResource(sharedTexture, __uuidof(ID3D11Resource), (void**)&res))) {
+		if (FAILED(g_d3d11Device->OpenSharedResource(g_sharedTexture, __uuidof(ID3D11Resource), (void**)&res))) {
 			MessageBoxA(NULL, "OpenSharedResource", NULL, MB_OK);
 			return 0;
 		}
 
-		if (FAILED(res->QueryInterface(__uuidof(ID3D11Texture2D), (void**)&d3d11Texture))) {
+		if (FAILED(res->QueryInterface(__uuidof(ID3D11Texture2D), (void**)&g_d3d11Texture))) {
 			MessageBoxA(NULL, "QueryInterface", NULL, MB_OK);
 			return 0;
 		}
 		//dont need the hook anymore
-		DWORD* dVtable = (DWORD*)d3d9Device;
+		DWORD* dVtable = (DWORD*)g_d3d9Device;
 		dVtable = (DWORD*)dVtable[0];
 		MH_DisableHook((DWORD_PTR*)dVtable[23]);
 		MH_RemoveHook((DWORD_PTR*)dVtable[23]);
@@ -163,21 +156,21 @@ LUA_FUNCTION(VRMOD_MirrorFrame) {
 		}
 	}
 
-	vr::Texture_t vrTexture = { d3d11Texture, vr::TextureType_DirectX, vr::ColorSpace_Auto };
+	vr::Texture_t vrTexture = { g_d3d11Texture, vr::TextureType_DirectX, vr::ColorSpace_Auto };
 	
 	vr::VRTextureBounds_t textureBounds;
 
 	//submit Left eye
-	textureBounds.uMin = 0.0f - horizontalOffset * 0.25f;
-	textureBounds.uMax = 0.5f - horizontalOffset * 0.25f;
+	textureBounds.uMin = 0.0f - g_horizontalOffset * 0.25f;
+	textureBounds.uMax = 0.5f - g_horizontalOffset * 0.25f;
 	textureBounds.vMin = 0.0f;
 	textureBounds.vMax = 1.0f;
 
 	vr::VRCompositor()->Submit(vr::EVREye::Eye_Left, &vrTexture, &textureBounds);
 
 	//submit Right eye
-	textureBounds.uMin = 0.5f + horizontalOffset * 0.25f;
-	textureBounds.uMax = 1.0f + horizontalOffset * 0.25f;
+	textureBounds.uMin = 0.5f + g_horizontalOffset * 0.25f;
+	textureBounds.uMax = 1.0f + g_horizontalOffset * 0.25f;
 	textureBounds.vMin = 0.0f;
 	textureBounds.vMax = 1.0f;
 
@@ -193,7 +186,7 @@ LUA_FUNCTION(VRMOD_Init) {
 
 	vr::HmdError error = vr::VRInitError_None;
 
-	pSystem = vr::VR_Init(&error, vr::VRApplication_Scene);
+	g_pSystem = vr::VR_Init(&error, vr::VRApplication_Scene);
 	if (error != vr::VRInitError_None) {
 		MessageBoxA(NULL, "VR_Init failed", NULL, MB_OK);
 		LUA->PushNumber(-1);
@@ -208,10 +201,10 @@ LUA_FUNCTION(VRMOD_Init) {
 
 	HookDirectX();
 
-	pSystem->GetRecommendedRenderTargetSize(&recommendedWidth, &recommendedHeight);
+	g_pSystem->GetRecommendedRenderTargetSize(&g_recommendedWidth, &g_recommendedHeight);
 
 	//get FOV and display offset values
-	vr::HmdMatrix44_t proj = pSystem->GetProjectionMatrix(vr::Hmd_Eye::Eye_Left, 1, 10);
+	vr::HmdMatrix44_t proj = g_pSystem->GetProjectionMatrix(vr::Hmd_Eye::Eye_Left, 1, 10);
 	float xscale = proj.m[0][0];
 	float xoffset = proj.m[0][2];
 	float yscale = proj.m[1][1];
@@ -220,36 +213,50 @@ LUA_FUNCTION(VRMOD_Init) {
 	float fov_nx = 2.0f * (atanf(fabsf((-1.0f - xoffset) / xscale)) * 180 / 3.141592654);
 	float fov_py = 2.0f * (atanf(fabsf((1.0f - yoffset) / yscale)) * 180 / 3.141592654);
 	float fov_ny = 2.0f * (atanf(fabsf((-1.0f - yoffset) / yscale)) * 180 / 3.141592654);
-	horizontalFOV = max(fov_px, fov_nx);
-	calculatedHorizontalOffset = -xoffset;
-	horizontalOffset = -xoffset;
+	g_horizontalFOV = max(fov_px, fov_nx);
+	g_calculatedHorizontalOffset = -xoffset;
+	g_horizontalOffset = -xoffset;
 
-	//prepare action related stuff (todo: handle actions dynamically)
+	//prepare action related stuff
 	char dir[256];
 	GetCurrentDirectory(256, dir);
 	char path[256];
 	sprintf_s(path, 256, "%s\\garrysmod\\lua\\bin\\vrmod_action_manifest.json", dir);
 
-	pInput = vr::VRInput();
-	if (pInput->SetActionManifestPath(path) != vr::VRInputError_None) {
-		MessageBoxA(NULL, "vrmod_action_manifest.json not found!", NULL, MB_OK);
+	g_pInput = vr::VRInput();
+	if (g_pInput->SetActionManifestPath(path) != vr::VRInputError_None) {
+		MessageBoxA(NULL, "SetActionManifestPath", NULL, MB_OK);
+		LUA->PushNumber(-1);
+		return 1;
 	}
-	pInput->GetActionSetHandle("/actions/vrmod", &actionSet);
-	pInput->GetActionHandle("/actions/vrmod/in/boolean_primaryfire", &boolean_primaryfire);
-	pInput->GetActionHandle("/actions/vrmod/in/boolean_secondaryfire", &boolean_secondaryfire);
-	pInput->GetActionHandle("/actions/vrmod/in/boolean_changeweapon", &boolean_changeweapon);
-	pInput->GetActionHandle("/actions/vrmod/in/boolean_spawnmenu", &boolean_spawnmenu);
-	pInput->GetActionHandle("/actions/vrmod/in/pose_lefthand", &pose_lefthand);
-	pInput->GetActionHandle("/actions/vrmod/in/pose_righthand", &pose_righthand);
-	pInput->GetActionHandle("/actions/vrmod/in/vector2_walkdirection", &vector2_walkdirection);
-	pInput->GetActionHandle("/actions/vrmod/in/boolean_walk", &boolean_walk);
-	pInput->GetActionHandle("/actions/vrmod/in/boolean_flashlight", &boolean_flashlight);
-	pInput->GetActionHandle("/actions/vrmod/in/boolean_turnleft", &boolean_turnleft);
-	pInput->GetActionHandle("/actions/vrmod/in/boolean_turnright", &boolean_turnright);
-	pInput->GetActionHandle("/actions/vrmod/in/boolean_use", &boolean_use);
-	pInput->GetActionHandle("/actions/vrmod/in/boolean_mic", &boolean_mic);
+	g_pInput->GetActionSetHandle("/actions/vrmod", &g_actionSet);
 
-	activeActionSet.ulActionSet = actionSet;
+	FILE * file = NULL;
+	fopen_s(&file, path, "r");
+	if (file == NULL) {
+		MessageBoxA(NULL, "failed to open vrmod_action_manifest.json", NULL, MB_OK);
+		LUA->PushNumber(-1);
+		return 1;
+	}
+
+	char word[256];
+	while (fscanf_s(file, "%*[^\"]\"%[^\"]\"", word, 256) == 1) {
+		if (strcmp(word, "name") == 0) {
+			if (fscanf_s(file, "%*[^\"]\"%[^\"]\"", g_actions[g_actionCount].fullname, 256) != 1)
+				break;
+			if (fscanf_s(file, "%*[^\"]\"type\"%*[^\"]\"%[^\"]\"", g_actions[g_actionCount].type, 256) != 1)
+				break;
+			g_actions[g_actionCount].name = g_actions[g_actionCount].fullname;
+			for (int i = 0; i < strlen(g_actions[g_actionCount].fullname); i++) {
+				if (g_actions[g_actionCount].fullname[i] == '/')
+					g_actions[g_actionCount].name = g_actions[g_actionCount].fullname + i + 1;
+			}
+			g_pInput->GetActionHandle(g_actions[g_actionCount].fullname, &(g_actions[g_actionCount].handle));
+			g_actionCount++;
+		}
+	}
+
+	g_activeActionSet.ulActionSet = g_actionSet;
 
 	LUA->PushNumber(0);
 	return 1;
@@ -260,12 +267,12 @@ LUA_FUNCTION(VRMOD_Init) {
 //*************************************************************************
 LUA_FUNCTION(VRMOD_Shutdown) {
 	vr::VR_Shutdown();
-	pSystem = NULL;
-	d3d9Device->Release();
-	d3d11Device->Release();
-	d3d11Context->Release();
-	d3d11Texture = NULL;
-	sharedTexture = NULL;
+	g_pSystem = NULL;
+	g_d3d9Device->Release();
+	g_d3d11Device->Release();
+	g_d3d11Context->Release();
+	g_d3d11Texture = NULL;
+	g_sharedTexture = NULL;
 	return 0;
 }
 
@@ -273,8 +280,8 @@ LUA_FUNCTION(VRMOD_Shutdown) {
 //                            LUA VRMOD_UpdatePoses
 //*************************************************************************
 LUA_FUNCTION(VRMOD_UpdatePoses) {
-	vr::VRCompositor()->WaitGetPoses(poses, vr::k_unMaxTrackedDeviceCount, NULL, 0);
-	pInput->UpdateActionState(&activeActionSet, sizeof(activeActionSet), 1);
+	vr::VRCompositor()->WaitGetPoses(g_poses, vr::k_unMaxTrackedDeviceCount, NULL, 0);
+	g_pInput->UpdateActionState(&g_activeActionSet, sizeof(g_activeActionSet), 1);
 	return 0;
 }
 
@@ -284,27 +291,25 @@ LUA_FUNCTION(VRMOD_UpdatePoses) {
 LUA_FUNCTION(VRMOD_GetPoses) {
 	vr::InputPoseActionData_t poseActionData;
 	vr::TrackedDevicePose_t pose;
-	char poseName[32];
+	char poseName[64];
 
 	LUA->CreateTable();
 	
-	for (int i = 0; i < 3; i++) {
+	for (int i = -1; i < g_actionCount; i++) {
 		//select a pose
 		poseActionData.pose.bPoseIsValid = 0;
 		pose.bPoseIsValid = 0;
-		if (i == 0) {
-			pose = poses[0];
+		if (i == -1) {
+			pose = g_poses[0];
 			memcpy(poseName, "hmd", 4);
 		}
-		else if (i == 1) {
-			pInput->GetPoseActionData(pose_lefthand, vr::TrackingUniverseStanding, 0, &poseActionData, sizeof(poseActionData), vr::k_ulInvalidInputValueHandle);
+		else if (strcmp(g_actions[i].type,"pose") == 0) {
+			g_pInput->GetPoseActionData(g_actions[i].handle, vr::TrackingUniverseStanding, 0, &poseActionData, sizeof(poseActionData), vr::k_ulInvalidInputValueHandle);
 			pose = poseActionData.pose;
-			memcpy(poseName, "lefthand", 9);
+			strcpy_s(poseName, 64, g_actions[i].name);
 		}
-		else if (i == 2) {
-			pInput->GetPoseActionData(pose_righthand, vr::TrackingUniverseStanding, 0, &poseActionData, sizeof(poseActionData), vr::k_ulInvalidInputValueHandle);
-			pose = poseActionData.pose;
-			memcpy(poseName, "righthand", 10);
+		else {
+			continue;
 		}
 		//
 		if (pose.bPoseIsValid) {
@@ -363,40 +368,26 @@ LUA_FUNCTION(VRMOD_GetActions) {
 
 	LUA->CreateTable();
 
-	LUA->PushBool((pInput->GetDigitalActionData(boolean_primaryfire, &digitalActionData, sizeof(digitalActionData), vr::k_ulInvalidInputValueHandle) == vr::VRInputError_None && digitalActionData.bState));
-	LUA->SetField(-2, "primaryfire");
-	LUA->PushBool((pInput->GetDigitalActionData(boolean_secondaryfire, &digitalActionData, sizeof(digitalActionData), vr::k_ulInvalidInputValueHandle) == vr::VRInputError_None && digitalActionData.bState));
-	LUA->SetField(-2, "secondaryfire");
-	LUA->PushBool((pInput->GetDigitalActionData(boolean_changeweapon, &digitalActionData, sizeof(digitalActionData), vr::k_ulInvalidInputValueHandle) == vr::VRInputError_None && digitalActionData.bState));
-	LUA->SetField(-2, "changeweapon");
-	LUA->PushBool((pInput->GetDigitalActionData(boolean_use, &digitalActionData, sizeof(digitalActionData), vr::k_ulInvalidInputValueHandle) == vr::VRInputError_None && digitalActionData.bState));
-	LUA->SetField(-2, "use");
-	LUA->PushBool((pInput->GetDigitalActionData(boolean_spawnmenu, &digitalActionData, sizeof(digitalActionData), vr::k_ulInvalidInputValueHandle) == vr::VRInputError_None && digitalActionData.bState));
-	LUA->SetField(-2, "spawnmenu");
-	LUA->PushBool((pInput->GetDigitalActionData(boolean_walk, &digitalActionData, sizeof(digitalActionData), vr::k_ulInvalidInputValueHandle) == vr::VRInputError_None && digitalActionData.bState));
-	LUA->SetField(-2, "walk");
-	LUA->PushBool((pInput->GetDigitalActionData(boolean_flashlight, &digitalActionData, sizeof(digitalActionData), vr::k_ulInvalidInputValueHandle) == vr::VRInputError_None && digitalActionData.bState));
-	LUA->SetField(-2, "flashlight");
-	LUA->PushBool((pInput->GetDigitalActionData(boolean_turnleft, &digitalActionData, sizeof(digitalActionData), vr::k_ulInvalidInputValueHandle) == vr::VRInputError_None && digitalActionData.bState));
-	LUA->SetField(-2, "turnleft");
-	LUA->PushBool((pInput->GetDigitalActionData(boolean_turnright, &digitalActionData, sizeof(digitalActionData), vr::k_ulInvalidInputValueHandle) == vr::VRInputError_None && digitalActionData.bState));
-	LUA->SetField(-2, "turnright");
-	LUA->PushBool((pInput->GetDigitalActionData(boolean_mic, &digitalActionData, sizeof(digitalActionData), vr::k_ulInvalidInputValueHandle) == vr::VRInputError_None && digitalActionData.bState));
-	LUA->SetField(-2, "mic");
-	LUA->PushBool((pInput->GetDigitalActionData(boolean_reload, &digitalActionData, sizeof(digitalActionData), vr::k_ulInvalidInputValueHandle) == vr::VRInputError_None && digitalActionData.bState));
-	LUA->SetField(-2, "reload");
-	LUA->PushBool((pInput->GetDigitalActionData(boolean_undo, &digitalActionData, sizeof(digitalActionData), vr::k_ulInvalidInputValueHandle) == vr::VRInputError_None && digitalActionData.bState));
-	LUA->SetField(-2, "undo");
-
-	LUA->CreateTable();
-
-	pInput->GetAnalogActionData(vector2_walkdirection, &analogActionData, sizeof(analogActionData), vr::k_ulInvalidInputValueHandle);
-	LUA->PushNumber(analogActionData.x);
-	LUA->SetField(-2, "x");
-	LUA->PushNumber(analogActionData.y);
-	LUA->SetField(-2, "y");
-
-	LUA->SetField(-2, "walkdirection");
+	for (int i = 0; i < g_actionCount; i++) {
+		if (strcmp(g_actions[i].type, "boolean") == 0) {
+			LUA->PushBool((g_pInput->GetDigitalActionData(g_actions[i].handle, &digitalActionData, sizeof(digitalActionData), vr::k_ulInvalidInputValueHandle) == vr::VRInputError_None && digitalActionData.bState));
+			LUA->SetField(-2, g_actions[i].name);
+		}
+		else if (strcmp(g_actions[i].type, "vector1") == 0) {
+			g_pInput->GetAnalogActionData(g_actions[i].handle, &analogActionData, sizeof(analogActionData), vr::k_ulInvalidInputValueHandle);
+			LUA->PushNumber(analogActionData.x);
+			LUA->SetField(-2, g_actions[i].name);
+		}
+		else if (strcmp(g_actions[i].type, "vector2") == 0) {
+			LUA->CreateTable();
+			g_pInput->GetAnalogActionData(g_actions[i].handle, &analogActionData, sizeof(analogActionData), vr::k_ulInvalidInputValueHandle);
+			LUA->PushNumber(analogActionData.x);
+			LUA->SetField(-2, "x");
+			LUA->PushNumber(analogActionData.y);
+			LUA->SetField(-2, "y");
+			LUA->SetField(-2, g_actions[i].name);
+		}
+	}
 
 	return 1;
 }
@@ -406,10 +397,10 @@ LUA_FUNCTION(VRMOD_GetActions) {
 //*************************************************************************
 LUA_FUNCTION(VRMOD_SetDisplayOffset) {
 	if (LUA->IsType(1, GarrysMod::Lua::Type::NUMBER)) {
-		horizontalOffset = LUA->GetNumber(1);
+		g_horizontalOffset = LUA->GetNumber(1);
 	}
 	else {
-		horizontalOffset = calculatedHorizontalOffset;
+		g_horizontalOffset = g_calculatedHorizontalOffset;
 	}
 	return 0;
 }
@@ -418,8 +409,8 @@ LUA_FUNCTION(VRMOD_SetDisplayOffset) {
 //                        LUA VRMOD_GetFOV
 //*************************************************************************
 LUA_FUNCTION(VRMOD_GetFOV) {
-	LUA->PushNumber(horizontalFOV);
-	LUA->PushNumber((float)recommendedWidth / (float)recommendedHeight); //aspect ratio
+	LUA->PushNumber(g_horizontalFOV);
+	LUA->PushNumber((float)g_recommendedWidth / (float)g_recommendedHeight); //aspect ratio
 	return 2;
 }
 
@@ -427,8 +418,8 @@ LUA_FUNCTION(VRMOD_GetFOV) {
 //                        LUA VRMOD_GetRecommendedResolution
 //*************************************************************************
 LUA_FUNCTION(VRMOD_GetRecommendedResolution) {
-	LUA->PushNumber(recommendedWidth*2); //return full res, not just one eye
-	LUA->PushNumber(recommendedHeight);
+	LUA->PushNumber(g_recommendedWidth*2); //return full res, not just one eye
+	LUA->PushNumber(g_recommendedHeight);
 	return 2;
 }
 
@@ -436,7 +427,7 @@ LUA_FUNCTION(VRMOD_GetRecommendedResolution) {
 //                           LUA VRMOD_GetIPD
 //*************************************************************************
 LUA_FUNCTION(VRMOD_GetIPD) {
-	vr::HmdMatrix34_t eyeToHeadRight = pSystem->GetEyeToHeadTransform(vr::Eye_Right); //units are in meters
+	vr::HmdMatrix34_t eyeToHeadRight = g_pSystem->GetEyeToHeadTransform(vr::Eye_Right); //units are in meters
 	LUA->PushNumber(eyeToHeadRight.m[0][3] * 2.0f); //ipd
 	LUA->PushNumber(eyeToHeadRight.m[2][3]); //z (how far back the eye is compared to hmd pos?)
 	return 2;
@@ -454,7 +445,7 @@ LUA_FUNCTION(VRMOD_HMDPresent) {
 //                        LUA VRMOD_GetVersion
 //*************************************************************************
 LUA_FUNCTION(VRMOD_GetVersion) {
-	LUA->PushNumber(4);
+	LUA->PushNumber(5);
 	return 1;
 }
 
