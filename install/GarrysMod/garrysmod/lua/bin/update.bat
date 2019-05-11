4baecf756f9cfa17194cd00cb9376acd19d80edb
Catse <45233053+catsethecat@users.noreply.github.com>
2019-05-11 03:03:57 +0300

    Rename update.bat to update_vrmod.bat

--- install/GarrysMod/garrysmod/lua/bin/update.bat
+++
@@ -1,1 +0,0 @@
-powershell -command "Invoke-WebRequest "https://github.com/catsethecat/vrmod-module/archive/master.zip" -Out vrmod.zip; Expand-Archive vrmod.zip -Force; Move-Item vrmod\vrmod-module-master\install\GarrysMod\garrysmod\lua\bin\* -Force; Remove-Item vrmod.zip; Remove-Item vrmod -Recurse"
