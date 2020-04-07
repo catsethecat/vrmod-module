1453a67a68dd73868b09aad6414f281e8077fbdd
Catse <45233053+catsethecat@users.noreply.github.com>
2020-04-08 02:59:37 +0300

    improved update script

--- install/GarrysMod/garrysmod/lua/bin/update_vrmod.bat
+++ install/GarrysMod/garrysmod/lua/bin/update_vrmod.bat
@@ -1,1 +1,3 @@
-powershell -command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest "https://github.com/catsethecat/vrmod-module/archive/master.zip" -Out vrmod.zip; Expand-Archive vrmod.zip -Force; Move-Item vrmod\vrmod-module-master\install\GarrysMod\garrysmod\lua\bin\* -Force; Remove-Item vrmod.zip; Remove-Item vrmod -Recurse"
+@echo off
+powershell -command [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest https://github.com/catsethecat/vrmod-module/archive/master.zip -Out vrmod.zip; Expand-Archive vrmod.zip -Force; Move-Item vrmod\vrmod-module-master\install\GarrysMod\garrysmod\lua\bin\* -Force; Remove-Item vrmod.zip; Remove-Item vrmod -Recurse; echo 'Update script finished, check above for errors.';
+@pause
