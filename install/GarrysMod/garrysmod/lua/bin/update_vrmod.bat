260af28422202a1a6c13deb825ef0c5e2ecb6885
Catse <45233053+catsethecat@users.noreply.github.com>
2021-05-02 03:38:44 +0300

    Delete update_vrmod.bat

--- install/GarrysMod/garrysmod/lua/bin/update_vrmod.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell -command [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest https://github.com/catsethecat/vrmod-module/archive/master.zip -Out vrmod.zip; Expand-Archive vrmod.zip -Force; Move-Item vrmod\vrmod-module-master\install\GarrysMod\garrysmod\lua\bin\* -Force; Remove-Item vrmod.zip; Remove-Item vrmod -Recurse; echo 'Update script finished, check above for errors.';
-@pause
